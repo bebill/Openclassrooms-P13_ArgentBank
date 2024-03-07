bb6daa3a5beb73d396834f9b335f0d69da9bfe28
Ben Bld <bld.prom@gmail.com>
2024-03-07 18:32:36 +0100

    chore: create header

---
+++ src/layout/Header.tsx
@@ -0,0 +1,24 @@
+import { Link, NavLink } from "react-router-dom";
+
+export const Header = () => {
+  return (
+    <header>
+      <nav className="main-nav">
+        <Link className="main-nav-logo" to="/">
+          <img
+            className="main-nav-logo-image"
+            src={`${process.env.PUBLIC_URL}/argentBankLogo.png`}
+            alt="Argent Bank Logo"
+          />
+          <h1 className="sr-only">Argent Bank</h1>
+        </Link>
+        <div>
+          <NavLink className="main-nav-item" to="#">
+            <i className="fa fa-user-circle"></i>
+             Sign In
+          </NavLink>
+        </div>
+      </nav>
+    </header>
+  );
+};
