7ba7f3c644451945ccc585842e6d118a6cb7cbab
Ben Bld <bld.prom@gmail.com>
2024-03-07 18:33:14 +0100

    chore: create footer

---
+++ src/layout/Footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <p className="footer-text">
+        Copyright {new Date().getFullYear()} Argent Bank
+      </p>
+    </footer>
+  );
+};
