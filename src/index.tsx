108c4973a7afdae29ccfc540cb0aee2c2b5465b8
Ben Bld <bld.prom@gmail.com>
2024-03-09 20:14:02 +0100

    chore: update index.tsx to use redux

--- src/index.tsx
+++ src/index.tsx
@@ -3,23 +3,32 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/utils/store";
 
 import { Header } from "./layout/Header";
-import { Home } from "./home";
+import { Home } from "./pages/home";
 import { Footer } from "./layout/Footer";
+import { Login } from "./pages/login";
+import { Profile } from "./pages/profile";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+          {/* <Route path="/*" element={<Error />} /> */}
+        </Routes>
+        <Footer />
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
 reportWebVitals();
