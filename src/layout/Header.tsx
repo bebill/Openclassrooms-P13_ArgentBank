b6f1d71d29a0da7b8e205be662ff8da2403701e8
Ben Bld <bld.prom@gmail.com>
2024-03-18 21:39:30 +0100

    chore: add sign out functionality

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,6 +1,21 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { logout } from "../redux/slices/authSlice";
+import { RootState } from "../redux/utils/selector";
 
 export const Header = () => {
+  const isAuth = useSelector((state: RootState) => state.auth.isAuth);
+  const firstName = useSelector(
+    (state: RootState) => state.profile.user.firstName
+  );
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    dispatch(logout());
+    navigate("/");
+  };
+
   return (
     <header>
       <nav className="main-nav">
@@ -13,10 +28,21 @@ export const Header = () => {
           <h1 className="sr-only">Argent Bank</h1>
         </Link>
         <div>
-          <NavLink className="main-nav-item" to="/login">
-            <i className="fa fa-user-circle"></i>
-             Sign In
-          </NavLink>
+          {isAuth ? (
+            <div className="main-nav-wrapper">
+              <i className="fa fa-user-circle"></i>
+              <NavLink className="main-nav-username" to="/profile">
+                {firstName}
+              </NavLink>
+              <button className="main-nav-signout" onClick={handleLogout}>
+                <i className="fa fa-sign-out"></i> Sign out
+              </button>
+            </div>
+          ) : (
+            <NavLink className="main-nav-signin" to="/login">
+              <i className="fa fa-user-circle"></i> Sign in
+            </NavLink>
+          )}
         </div>
       </nav>
     </header>
